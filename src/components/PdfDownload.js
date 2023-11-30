735bcea0997b29e0834702b4ff29e70abb76e315
ThanujaReddyKM <thanuja.k@ibirdsservices.com>
2023-11-30 18:06:48 +0530

    Pdf_Changes_30-11-23(Google view, Property Image)

--- src/components/PdfDownload.js
+++ src/components/PdfDownload.js
@@ -1,17 +1,11 @@
 import React from 'react'
-import { Document,Text,View, Page, Image, StyleSheet } from '@react-pdf/renderer';
+import { Document,Text,Link,View, Page, Image, StyleSheet } from '@react-pdf/renderer';
 
 const styles = StyleSheet.create({
     body :{
         // backgroundColor: '#07575B',
         backgroundColor: '#132925',
     },
-    // body :{
-    //     paddingTop: 10,
-    //     paddingBottom: 10,
-    //     paddingHorizontal: 35,
-    //     backgroundColor: '#07575B',
-    // },
     size :{
         height:320,
         width:620,
@@ -59,31 +53,16 @@ const styles = StyleSheet.create({
     twoColumnContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    paddingHorizontal: 1,
-    paddingVertical: 1,
+    // paddingHorizontal: 1,
+    // paddingVertical: 1,
   },
 
   column: {
     // backgroundColor:'#FFF59D',
     backgroundColor:'#B7A292',
-
-    //flex: 1,
+    flex: 1,
     // backgroundColor:'white',
      //marginVertical: 5,
-  },
-  columnLine:{
-    flex: 1,
-    marginVertical: 5,
-    borderRightColor: '#666',
-    borderRightWidth: 2,
-  },
-
-  columnHeader: {
-    backgroundColor: '#333',
-    color: 'white',
-    fontWeight: 'bold',
-    paddingVertical: 5,
-    paddingHorizontal: 10,
   },
 
   columnRow: {
@@ -102,6 +81,7 @@ const styles = StyleSheet.create({
     color: 'black',
     fontSize:10
   },
+
   details:{
     fontSize:12,
     color:'white',
@@ -117,31 +97,82 @@ const styles = StyleSheet.create({
     marginTop:20
   },
   cityOverviewImage:{
-    height:350,
-    marginBottom:30,
-    marginTop:30,
+    height:260,
+     marginTop:30,
+     marginBottom:30,
+
     marginLeft:10,
     marginRight:10
 },
-centerLine: {
-    height: '200%',
-    width: 1,
-    backgroundColor: 'black',
-  },
+
   dataBox: {
     border: '0.6pt solid #A27C4B',
     borderRadius: 0.5,
     // marginVertical: 5,
   },
+  googleView :{
+    color: 'black', 
+    fontSize: 34, 
+    lineHeight: 1.5, 
+    display: 'flex',
+     marginLeft:180
+  },
+  logoImage:{ 
+     height: 42, 
+     width: 110,
+     marginRight:10,
+      marginBottom:5,
+     marginLeft:220
+    },
+    googleImage:{
+  height: 230, 
+  width:550, 
+  marginTop: 10, 
+  marginBottom: 10, 
+//   marginLeft: 20, 
+//   marginRight: 20 
+},
+
+detailsProperty:{
+    fontSize:12,
+    color:'white',
+    fontWeight:'bold',
+    // marginLeft:90
+  },
+
+  propertyColumnRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+     alignItems: 'center', 
+    borderBottomColor: '#A27C4B',
+    borderBottomWidth: 1,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+
+  propertyColumnRowText: {
+     alignItems:'center',
+     justifyContent:'center',
+    display:'flex',
+    color: 'black',
+    fontSize:8
+  },
+  propertyRow1Colour:{
+    backgroundColor:'#C8CABE'
+  },
+  propertyRow2Colour:{
+    backgroundColor:'#F7F7F7'
+  }
+    
 });
 
 console.log('Background color:', styles.body.backgroundColor);
 const PdfDownload = () => {
     return (
-
-
   <Document>
-<Page size={styles.size} style={{ ...styles.body, flexDirection: 'row', }}>
+
+
+      <Page size={styles.size} style={{ ...styles.body, flexDirection: 'row', }}>
 <View style={{ flex: 7 , alignItems:'center',paddingTop:10, }}>
   <Text style={styles.centeredText}>Proposed Option</Text>
   <Text style={styles.centeredText}>For</Text>
@@ -171,9 +202,7 @@ const PdfDownload = () => {
     <View style={styles.dataBox}>
       <View style={{...styles.columnRow,backgroundColor:'#A27C4B'}}>
       <Text style={{...styles.details}}>Details</Text>
-        {/* <Text style={{...styles.columnRowText,fontSize:12,color:'white',fontWeight:'bold',marginLeft:90}}>Details</Text> */}
       </View>
-      {/* <View style={styles.columnLine}> */}
       <View style={styles.columnRow}>
         <Text style={styles.columnRowText}>State</Text>
         <Text style={styles.columnRowText}>Rajasthan</Text>
@@ -200,7 +229,6 @@ const PdfDownload = () => {
         <Text style={styles.columnRowText}>Population Density </Text>
         <Text style={{...styles.columnRowText,marginLeft:-150,marginTop:12}}> (2023)</Text>
         <Text style={styles.columnRowText}>27,613/KM{String.fromCharCode(0x00B2)}</Text>
-        {/* <Text style={styles.columnRowText}>27,613/KM2</Text> */}
       </View>
       <View style={styles.columnRow}>
         <Text style={styles.columnRowText}>Average literacy rate</Text>
@@ -211,32 +239,125 @@ const PdfDownload = () => {
     <Text style={{...styles.columnRowText,marginLeft:-150,marginTop:12}}>Income(District)</Text>
     <Text style={styles.columnRowText}>Rs 1,56,150</Text>
     </View>
-      {/* <View style={styles.columnRow}>
-        <Text style={styles.columnRowText}>Annual Per Capita Income(District)</Text>
-        <Text style={styles.columnRowText}>Rs 1,56,150</Text>
-      </View> */}
-      {/* </View> */}
     </View>
     </View>
-
     </View>
         <View style={{ flex: 6 }}>
-            {/* <Image style={{...styles.image}} src="image5.jpg" /> */}
             <Image style={styles.cityOverviewImage} src="image1.jpg" />
 
         </View>
   </View>
 </Page>
 
-
-      <Page size={styles.size}  style={styles.body} >
+<Page size={styles.size}  style={styles.body}>
+<View style={{flexDirection: 'row',  alignItems: 'center', paddingTop: 10, backgroundColor: '#B7A292' }}>
+    <Text style={styles.googleView}>Google View</Text>
+    <Image style={{...styles.size ,...styles.logoImage}} src="logo.jpg" />
+  </View>
+  <View style={{ flex: 5, alignItems: 'center' }}>
+    <Image style={{...styles.size , ...styles.googleImage }} src="google.jpg" />
+  </View>
       </Page>
 
       <Page size={styles.size}  style={styles.body}>
-      </Page>
+<View style={{flexDirection: 'row',  alignItems: 'center', paddingTop: 10, backgroundColor: '#B7A292' }}>
+    <Text style={styles.googleView}>Property Images</Text>
+    <Image style={{...styles.size ,...styles.logoImage}} src="logo.jpg" />
+  </View>
 
-      <Page size={styles.size} style={styles.body} >
+  <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+  <View style={{ flex: 2, marginTop: 12, marginLeft: 8, marginRight: 6, flexDirection: 'column', justifyContent: 'space-between' }}>
+  <View style={{ ...styles.dataBox, backgroundColor: '#B7A292', height: 246 }}>
+      <View style={{...styles.propertyColumnRow,backgroundColor:'#A27C4B'}}>
+      <Text style={{...styles.detailsProperty,marginLeft:15}}>Parameters</Text>
+      <Text style={{...styles.detailsProperty,marginRight:55}}>Details</Text>
+      </View>
+      <View style={{...styles.propertyColumnRow,...styles.propertyRow1Colour}}>
+        <Text style={{...styles.propertyColumnRowText,marginLeft:25  }}>Floor Plate</Text>
+        <Text style={{...styles.propertyColumnRowText ,marginTop:-20, marginLeft:120}}>Basement - 2020 sq.ft</Text>
+        <Text style={{...styles.propertyColumnRowText,marginTop:1 , marginLeft:-15 }}>Ground Floor - 2245 sq. ft</Text>
+        <Text style={{...styles.propertyColumnRowText,marginTop:24,marginRight:105 }}>1st Floor - 3170 sq. ft</Text>
+
+
+      </View>
+      <View style={{...styles.propertyColumnRow,...styles.propertyRow2Colour}}>
+        <Text style={{...styles.propertyColumnRowText,marginLeft:-5 }}>Expected Date of K=HOTP</Text>
+        <Text style={{...styles.propertyColumnRowText,marginRight:50 }}>4 Months</Text>
+      </View>
+      <View style={{...styles.propertyColumnRow,...styles.propertyRow1Colour}}>
+        <Text style={{...styles.propertyColumnRowText,marginLeft:25 }}>Rental</Text>
+        <Text style={{...styles.propertyColumnRowText,marginRight:35 }}>To be discussed</Text>
+      </View>
+      <View style={{...styles.propertyColumnRow,...styles.propertyRow2Colour}}>
+        <Text style={{...styles.propertyColumnRowText,marginLeft:25 }}>Frontage </Text>
+        <Text style={{...styles.propertyColumnRowText,marginRight:55 }}>41.3ft.</Text>
+      </View>
+            <View style={{...styles.propertyColumnRow,...styles.propertyRow1Colour}}>
+        <Text style={{...styles.propertyColumnRowText,marginLeft:25 }}>Location</Text>
+        <Link src="https://maps.app.goo.gl/oCeBr58yKe38Qp1F9">
+            <Text style={{...styles.propertyColumnRowText, color: 'blue' , marginRight:-15}}>
+            https://maps.app.goo.gl/oCeBr58yKe38Qp1F9
+            </Text>
+        </Link>
+        </View>
+      <View style={{...styles.propertyColumnRow,...styles.propertyRow2Colour}}>
+        <Text style={{...styles.propertyColumnRowText,marginLeft:25 }}>Tenure</Text>
+        <Text style={{...styles.propertyColumnRowText,marginRight:55 }}>12 yrs</Text>
+      </View>
+    </View>
+    </View>
+
+      <View style={{ flex: 2, marginTop: 12, marginLeft: 6, marginRight: 8, flexDirection: 'column', justifyContent: 'space-between' }}>
+      <View style={{ ...styles.dataBox, backgroundColor: '#B7A292', height: 246 }}>
+      <View style={{...styles.propertyColumnRow,backgroundColor:'#A27C4B'}}>
+      <Text style={{...styles.detailsProperty,marginLeft:15}}>Parameters</Text>
+      <Text style={{...styles.detailsProperty,marginRight:55}}>Details</Text>
+      </View>
+      <View style={{...styles.propertyColumnRow,...styles.propertyRow1Colour}}>
+        <Text style={{...styles.propertyColumnRowText,marginLeft:10  }}>Rent Excalation</Text>
+        <Text style={{...styles.propertyColumnRowText , marginLeft:30}}>15% after every three years of last paid rent</Text>
+      </View>
+      <View style={{...styles.propertyColumnRow,...styles.propertyRow2Colour}}>
+        <Text style={{...styles.propertyColumnRowText, }}>Maintenance Charges</Text>
+        <Text style={{...styles.propertyColumnRowText,marginRight:50 }}>As per actual</Text>
+      </View>
+      <View style={{...styles.propertyColumnRow,...styles.propertyRow1Colour}}>
+        <Text style={{...styles.propertyColumnRowText, }}>Electric Connection</Text>
+        <Text style={{...styles.propertyColumnRowText,marginRight:35 }}>Available as per requirement</Text>
+      </View>
+      <View style={{...styles.propertyColumnRow,...styles.propertyRow2Colour}}>
+        <Text style={{...styles.propertyColumnRowText,marginLeft:15 }}>Taxes </Text>
+        <Text style={{...styles.propertyColumnRowText,marginTop:-12, marginLeft:80}}>Any Taxes on th property are to be borne by the</Text>
+         <Text style={{...styles.propertyColumnRowText,marginTop:10 , marginRight:51}}>landlord. Any taxes on rental like GST. are to be</Text>
+        <Text style={{...styles.propertyColumnRowText,marginTop:28,marginRight:105 }}>paid by the company</Text>
+      </View>
+      <View style={{...styles.propertyColumnRow,...styles.propertyRow1Colour}}>
+        <Text style={{...styles.propertyColumnRowText,marginTop:-5,marginLeft:-14}}>Stamp Duty and Registration</Text>
+        <Text style={{...styles.propertyColumnRowText,marginTop:12 ,marginRight:95}}>Cos</Text>
+        <Text style={{...styles.propertyColumnRowText,marginRight:100 }}>50:50</Text>
+      </View>
+      <View style={{...styles.propertyColumnRow,...styles.propertyRow2Colour}}>
+        <Text style={{...styles.propertyColumnRowText,marginLeft:25 }}>status</Text>
+        <Text style={{...styles.propertyColumnRowText,marginRight:55 }}>Commercial approved</Text>
+      </View>
+    
+      <View style={{...styles.propertyColumnRow,...styles.propertyRow1Colour}}>
+        <Text style={{...styles.propertyColumnRowText, }}>Neighbouring Brands</Text>
+        <Text style={{...styles.propertyColumnRowText, marginLeft:90}}>Nike,Puma,Adida,Starbucks,NikemCanabil,Us</Text>
+        <Text style={{...styles.propertyColumnRowText,marginRight:105,marginTop:18 }}>polo,Titan,Iconic,Levi's etc..</Text>
+
+      </View>
+    </View>
+    
+  </View>
+  </View>
       </Page>
+{/* 
+      <Page size={styles.size}  style={styles.body}>
+      <Text style={{ color: 'black', fontSize: 34, lineHeight: 1.5, display: 'flex', marginLeft:180 }}>Checking 2</Text>
+      </Page> */}
+
+     
 
       <Page size={styles.size} style={styles.body}>
       </Page>
